1b4bfa016b045972d5a6eae54cd8728433c44091
DanteWHS <dante130283@student-schoolsofwestfield.org>
2024-09-13 12:06:05 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,48 +1,15 @@
 
-/* Global variables */
-var who = "wooden shoe";
-var joke = "Wooden shoe like to know!";
-var expected = who + " who?";
+
+var answer = prompt;
+var yes = "yes";
 
 function custom(){
 	prompt("why did you press that");
 	prompt("do you just press any button you see?");
-	if (answer=="yes") prompt("I'm sure that hasn't caused any problems");
-	else
-
+	if (answer == "yes") alert("I'm sure that hasn't caused any problems");
+	else prompt("wanna play tic tac toe?");
+}
 
 function sayHello(){
 	alert("Hello, world!");
 }
-/* Simple version */
-function knock(){
-  prompt("Knock Knock!");
-  prompt(who);
-  alert(joke);
-}
-
-/* Better */
-function knockCheck(){
-  prompt("Knock Knock!");
-  var answer = prompt(who);
-   expected = who + " who?";
-  if (answer == expected) alert(joke);
-  else alert("Huh?");
-}
-
-/* Best */
-function knockAgain(){
-  prompt("Knock Knock!");
-  var answer = prompt(who);
-  if (answer == expected) alert(joke);
-  if (answer == joke) alert("Bruh");
-  // this next line is "recursion". 
-  else askAgain();
-}
-
-function askAgain() {
-  alert("Huh?");
-  var answer = prompt(who);
-  // what is the difference between = and ==?
-  if (answer == expected) alert(joke);
-  else askAgain();
